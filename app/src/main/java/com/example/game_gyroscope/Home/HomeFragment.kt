72c758d37a7920f58da6b5f4e53ae95ae2e54685
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2021-07-30 14:17:18 +0430

    A little change on activity_main.xml and fragment_home.xml and HomeFragment.kt

--- app/src/main/java/com/example/game_gyroscope/Home/HomeFragment.kt
+++ app/src/main/java/com/example/game_gyroscope/Home/HomeFragment.kt
@@ -1,11 +1,19 @@
 package com.example.game_gyroscope.Home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.game_gyroscope.R
 
 class HomeFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn = view.findViewById<Button>(R.id.dynamicButton)
+        btn.translationX = 100f
+        btn.translationY = 100f
+
+    }
 
 }
