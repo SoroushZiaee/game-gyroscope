89216c98904367a630a592c0430da630cb641f14
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2021-07-30 13:41:46 +0430

    Create the first fragment of project named home

---
+++ app/src/main/java/com/example/game_gyroscope/Home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.game_gyroscope.Home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+
+}
