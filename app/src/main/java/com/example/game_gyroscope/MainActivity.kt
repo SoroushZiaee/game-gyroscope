10ddb99cc0d9fb3d473d1666bc2835d94f5cdea2
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2021-07-31 22:58:53 +0430

    create the animation with gravity sensor

---
+++ app/src/main/java/com/example/game_gyroscope/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.game_gyroscope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.game_gyroscope.Register.RegisterFragment
+import java.lang.NullPointerException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
