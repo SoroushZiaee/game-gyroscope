00cd118bd1d8985fb40b5ce740078eb8df927394
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2021-07-31 22:54:08 +0430

    create the animation with gravity sensor

--- app/src/main/java/com/example/game_gyroscope/Home/HomeFragment.kt
+++ app/src/main/java/com/example/game_gyroscope/Home/HomeFragment.kt
@@ -1,19 +1,56 @@
 package com.example.game_gyroscope.Home
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.game_gyroscope.R
+import java.util.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home), SensorEventListener {
+    private var sensorManager: SensorManager ? = null
+    private var sensor: Sensor ? = null
+    private var btn: Button ? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager?.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        btn = view.findViewById<Button>(R.id.dynamicButton)
+    }
 
-        val btn = view.findViewById<Button>(R.id.dynamicButton)
-        btn.translationX = 100f
-        btn.translationY = 100f
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        print("Here")
+        if(event == null){
+            return
+        }
+
+        val x = event.values[0]
+        val y = event.values[1]
+
+        btn?.translationY = y
+        btn?.translationX = x
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
     }
 
 }
+
--- app/src/main/java/com/example/game_gyroscope/Register/RegisterFragment.kt
+++ app/src/main/java/com/example/game_gyroscope/Register/RegisterFragment.kt
@@ -1,9 +1,42 @@
 package com.example.game_gyroscope.Register
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-
+import androidx.navigation.fragment.findNavController
+import com.example.game_gyroscope.R
 
 
 class RegisterFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn = view.findViewById<Button>(R.id.button)
+        btn.setOnClickListener { navigateHome() }
+    }
+
+    private fun navigateHome(){
+        findNavController().navigate(
+            RegisterFragmentDirections.actionRegisterFragmentToHomeFragment()
+        )
+    }
 
 }
